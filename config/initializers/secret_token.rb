a77f52ebc86bc895de4fe02ddd8778d7dd2bc65b
b0hne1337 <b0hne1337@web.de>
2014-02-23 23:06:23 +0100

    Add clean rails 3.2.17 app with pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sr::Application.config.secret_token = 'a22de647af5cf51e707095f159ec4b3a47cf44481661563386af9c5caa794823bd23492bedd572c4342f8ed8dd8e990096d985408002257a1968c0d771cf34ad'
